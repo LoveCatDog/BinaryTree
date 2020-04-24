a20ad75f01af4db775ad7bdde7672210a3b2187c
LoveCatDog <1430363821@qq.com>
2020-04-24 16:31:11 +0800

    update

--- tengxunTest.js
+++ tengxunTest.js
@@ -33,6 +33,9 @@ function getStringLength(str) {
     var l = str.length;
     var leng = 0;
     for (i = 0; i < l; i++) {
+        /**
+         * charCodeAt() 方法可返回指定位置的字符的 Unicode 编码,例如，可用来判断在I处的字符是中文还是英文！
+         */
         if ((str.charCodeAt(i) & 0xff00) != 0) {
             leng++;
         }
@@ -43,18 +46,27 @@ function getStringLength(str) {
 
 /**
  * 4. js题目：实现一个js方法appendParams，可以给url加上多个新的参数，多考虑兼容性。
-用法例如appendParams('http://url', {a:1,b:2})返回http://url?a=1&b=2。
+用法例如appendParams('http://url', {a:1,b:2})返回http://url?a=1&b=2。 若url包含？ 
+ *
  * @param {*} url 
  * @param {*} obj 
  */
+// TODO 哈希（#)
+
 
 function appendParams(url, obj) {
     let newArray = []
     for (let [key, value] of Object.entries(obj)) {
         newArray.push(key + '=' + value)
     }
-    console.log(newArray.join('&'))
-    return url + newArray
+    newArray = newArray.join('&')
+    // ?
+    if (url.indexOf('?') > -1) {
+        return url + newArray
+    } else {
+        return url + '?' + newArray
+    }
+
 }
 
 /**
@@ -62,10 +74,10 @@ function appendParams(url, obj) {
  * @param {*} nowTime 时间戳
  */
 
-function getTime(ATime) {
-    let nowTimeDate = new Date();
-    nowTimeDate.setHours(0, 0, 0, 0);
-    var realTime = nowTimeDate.getTime() + ATime;
+function getTime(timer) {
+    let nowTimeDate = new Date(timer);
+    let newTimeDate = nowTimeDate.setHours(0, 0, 0, 0); //获取该天的第一天0点0分0秒的时间
+    let realTime = new Date(newTimeDate).getTime();
     return realTime
 }
 
@@ -75,11 +87,11 @@ function getTime(ATime) {
  */
 
 var pubsub = (function () {
-    var obj = {}
+    var q = {}
     topics = {},
         subUid = -1;
-    //发布事件
-    obj.publish = function (topic, args) {
+    //发布消息
+    q.publish = function (topic, args) {
         if (!topics[topic]) {
             return;
         }
@@ -91,7 +103,7 @@ var pubsub = (function () {
         return this;
     };
     //订阅事件
-    obj.subscribe = function (topic, func) {
+    q.subscribe = function (topic, func) {
         topics[topic] = topics[topic] ? topics[topic] : [];
         var token = (++subUid).toString();
         topics[topic].push({
@@ -100,12 +112,15 @@ var pubsub = (function () {
         });
         return token;
     };
-
-    return obj;
+    return q;
 })();
-
-var message = function (topics, data) {
-    console.log(topics + ":" + data);
+//触发的事件
+var logmsg = function (topics, data) {
+    console.log("logging:" + topics + ":" + data);
 }
-
-var sub1 = pubsub.subscribe(message);
+//监听指定的消息'msgName'
+var sub = pubsub.subscribe('msgName', logmsg);
+//发布消息'msgName'
+pubsub.publish('msgName', 'hello world');
+//发布无人监听的消息'msgName1'
+pubsub.publish('anotherMsgName', 'me too!');
